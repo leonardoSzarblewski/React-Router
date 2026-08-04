cb97ece9bdea01974a958236beb9b0e497f17fe3
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-04 14:46:44 -0300

    Add card layout for products and display product links in Products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -14,6 +14,14 @@ export default function Products() {
           Categoria <strong>{category}</strong>
         </span>
       )}
+
+      <div className="cards">
+        <a href="/details/1">Produto 1</a>
+        <a href="/details/2">Produto 2</a>
+        <a href="/details/3">Produto 3</a>
+        <a href="/details/4">Produto 4</a>
+        <a href="/details/5">Produto 5</a>
+      </div>
     </div>
   );
 }
