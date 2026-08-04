458027ff826824d1f96453070bd2f6eb88f804cf
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-08-04 14:53:40 -0300

    Add navigation button to return to previous page in Details component

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,10 +1,19 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 export default function Details() {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  function returnPage() {
+    navigate(-1);
+  }
 
   return (
     <div>
+      <button type="button" onClick={returnPage}>
+        Voltar
+      </button>
+
       <h1>Detalhes</h1>
 
       <span>
